af642242c2a121b4f80d58f10ee2cdce9c1f8ead
Allaye <kolade199@gmail.com>
2021-12-19 12:18:05 +0100

    constructing the Replyemail class

--- utilizer/utility.py
+++ utilizer/utility.py
@@ -1,6 +1,10 @@
 import imaplib
 
 
-class Util(imaplib.IMAP4_SSL):
-    def __init__(self):
-        super().__init__()
+class ReplyMsg(imaplib.IMAP4_SSL):
+    def __init__(self, imap_server, sender_email, sender_pwd, ssl=None):
+        super().__init__(imap_server)
+        self.host = imap_server
+        self.sender = sender_email
+        self.sender_pwd = sender_pwd
+        self.ssl = ssl
