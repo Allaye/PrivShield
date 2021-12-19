3514d5b0db03a31a6a5cb6f6ed2caa3aacfffe47
Allaye <kolade199@gmail.com>
2021-12-19 17:47:41 +0100

    implemented login and logout feature

--- utilizer/utility.py
+++ utilizer/utility.py
@@ -2,9 +2,21 @@ import imaplib
 
 
 class ReplyMsg(imaplib.IMAP4_SSL):
-    def __init__(self, imap_server, sender_email, sender_pwd, ssl=None):
-        super().__init__(imap_server)
-        self.host = imap_server
-        self.sender = sender_email
-        self.sender_pwd = sender_pwd
-        self.ssl = ssl
+
+    def __init__(self, host):
+        super(ReplyMsg, self).__init__(host)
+
+    def signout(self):
+        try:
+            return self.logout()
+        except Exception as e:
+            return e
+
+    def login(self, email: str, pwd: str):
+        try:
+            return self.login(email, pwd)
+        except Exception as e:
+            return e
+
+
+
