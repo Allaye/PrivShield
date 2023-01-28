63303ba321c893c9dcb9f394d7e0f292b9f9c5f4
allaye <kolade199@gmail.com>
2023-01-28 21:46:42 +0000

    update shield class

--- shield/shield.py
+++ shield/shield.py
@@ -36,4 +36,34 @@ class Shield:
         except FileNotFoundError:
             click.echo(click.style("\nmake sure key-guard is initialized\n", fg='red'))
 
-    
+    def search(self, path, fileignore_path, keyignore_path):
+        """
+        search text method used in scanning
+
+        """
+        file_contents = self.load_paths(fileignore_path,
+                                        keyignore_path)
+        os.chdir('..')
+        os.chdir(path)
+        path = os.listdir(path)
+        for fname in path[::-1]:
+            abs_path = os.path.abspath(fname)
+            if os.path.isfile(abs_path):
+                try:
+                    if os.path.basename(fname) not in file_contents["exempted_files"] and not os.path.basename(
+                            fname).startswith('.'):
+                        for l_no, line in enumerate(open(fname, 'r')):
+                            for word in file_contents["guarded_words"]:
+                                if word in line:
+                                    click.echo(click.style(
+                                        f"Warning: '{word}' found in `{fname.split('/')[-1]}` Kindly check for line: `{l_no}` -> '{line.strip()}' to make sure you don't commit sensitive data.",
+                                        fg='yellow'))
+
+                        path.remove(os.path.basename(fname))
+                except UnicodeDecodeError:
+                    pass
+            elif os.path.isdir(abs_path) and not os.path.basename(abs_path).startswith('.'):
+                print(f"Scanning {abs_path}")
+                self.search(abs_path, fileignore_path, keyignore_path)
+                path.remove(os.path.basename(abs_path))
+                os.chdir('..')
