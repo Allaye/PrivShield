1ceb0914811e3c578ee193bde0f5d960d8abb72d
allaye <kolade199@gmail.com>
2023-01-29 15:34:44 +0000

    minor upddates

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 from setuptools import setup, find_packages
-
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 with open("requirements.txt", "r", encoding="utf-8") as fh:
