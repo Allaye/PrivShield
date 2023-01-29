68c7d2d7eb8444223b700add30109b62c5d79837
allaye <kolade199@gmail.com>
2023-01-29 15:34:23 +0000

    minor upddates

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+with open("requirements.txt", "r", encoding="utf-8") as fh:
+    requirements = fh.read()
 setup(
     name='key_guard',
     version='0.0.1a',
